a94f00dab9e1fa8a32f2067d5c69154c86ee276d
Victor Iglesias <viglesiasalcon@medallia.com>
2024-05-06 16:08:27 +0200

    1.1.5

--- Package.swift
+++ Package.swift
@@ -13,25 +13,34 @@ let package = Package(
     products: [
         .library(
             name: "sdk-a",
-            targets: ["SDK_A"]
+            targets: ["SDK_A", "SDK_BR"]
         ),
     ],
-    dependencies: [
-        .package(
-            name: "sdk-br",
-            url: "https://github.com/DecibelSDK/SDK_BR.git",
-            from: "1.0.0"
-        ),
-    ],
+//    dependencies: [
+//        .package(
+//            name: "sdk-br",
+//            url: "https://github.com/DecibelSDK/SDK_BR.git",
+//            from: "1.0.0"
+//        ),
+//    ],
     targets: [
         .binaryTarget(
             name: "SDK_A",
             path: "SDK_A.xcframework"
         ),
-        .target(
-            name: "SDK_A",
-            dependencies: ["sdk-br"],
-            path: "SDK_A.xcframework"
-        )
+        .binaryTarget(
+            name: "SDK_BR",
+            url: "https://www.icloud.com/iclouddrive/0e5txnfh_Pwa58VU8cY-VX-cw#SDK%5FBR.xcframework",
+            checksum: "5bc54cca3f15b7dea50e83266475e9fcda53777e2856abb5a9c9617533c98cf2"
+        ),
+//        .target(
+//            name: "FrameworkCTargets",
+//            dependencies: [
+//                .target(name: "SDK_A", condition: .when(platforms: .some([.iOS]))),
+//                .product(name: "SDK_BR", package: "sdk-br", condition: .when(platforms: .some([.iOS])))
+//               .target(name: "FrameworkC", condition: .when(platforms: .some([.iOS])))
+//            ],
+//            path: "FrameworkCTargets"
+//        )
     ]
 )
