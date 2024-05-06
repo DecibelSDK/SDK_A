a614cbb67c247fcd71f82ad793e13803326260ad
Victor Iglesias <viglesiasalcon@medallia.com>
2024-05-06 12:35:42 +0200

    1.0.2

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             name: "sdk-a",
             targets: ["SDK_A"]
         ),
-    ], 
+    ],
     dependencies: [
         .package(
             name: "sdk-br",
@@ -27,6 +27,11 @@ let package = Package(
         .binaryTarget(
             name: "SDK_A",
             path: "SDK_A.xcframework"
+        ),
+        .target(
+            name: "sdk-a",
+            dependencies: ["sdk-br"],
+            path: "/"
         )
     ]
 )
