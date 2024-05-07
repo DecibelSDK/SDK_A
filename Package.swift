a015d4564d021f934751106ff83047c473d665ff
Victor Iglesias <viglesiasalcon@medallia.com>
2024-05-07 16:15:58 +0200

    1.2.4

--- Package.swift
+++ Package.swift
@@ -1,46 +1,46 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-//  
+//
 //  Copyright © 2023 Medallia. Use subject to licensing terms.
 
 import PackageDescription
 
-    let package = Package(
-        name: "sdk-a",
-        platforms: [
-            .iOS(.v11)
-        ],
-        products: [
-            .library(
-                name: "sdk-a",
-                targets: ["SDK_A_Wrapper"]
-            )
-        ],
-        dependencies: [
-            .package(
-                url: "https://github.com/DecibelSDK/SDK_BR.git",
-                from: "1.0.2"
-            )
-        ],
-        targets: [
-            .binaryTarget(
-                name: "SDK_A",
-                path: "SDK_A.xcframework"
-            ),
-            .target(
-                name: "SDK_A_Wrapper",
-                dependencies: [
-                    .target(
-                        name: "SDK_A",
-                        condition: .when(platforms: [.iOS])
-                    ),
-                    .product(
-                        name: "sdk-br",
-                        package: "SDK_BR",
-                        condition: .when(platforms: [.iOS])
-                    )
-                ],
-                path: "SDK_A_Wrapper"
-            )
-        ]
-    )
+let package = Package(
+    name: "sdk-a",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "sdk-a",
+            targets: ["SDK_A_Wrapper"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/DecibelSDK/SDK_BR.git",
+            from: "1.0.2"
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SDK_A",
+            path: "SDK_A.xcframework"
+        ),
+        .target(
+            name: "SDK_A_Wrapper",
+            dependencies: [
+                .target(
+                    name: "SDK_A",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .product(
+                    name: "sdk-br",
+                    package: "SDK_BR",
+                    condition: .when(platforms: [.iOS])
+                )
+            ],
+            path: "SDK_A_Wrapper"
+        )
+    ]
+)
